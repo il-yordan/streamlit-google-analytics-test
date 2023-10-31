fde480052e6ad463a2dfe9c7eee3bcb72106632a
yordankitov <yordankitov@infinitelambda.com>
2023-10-31 15:20:02 +0200

    changed page

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+import streamlit.components.v1 as components
+
 st.title('Uber pickups in NYC')
 
 DATE_COLUMN = 'date/time'
@@ -34,3 +36,64 @@ filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 
 st.subheader('Map of all pickups at %s:00' % hour_to_filter)
 st.map(filtered_data)
+
+st.components.v1.html('''
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+  <title>Test App</title>
+
+
+
+  <!-- Google Verification -->
+  <meta name="google-site-verification" content="m_mBYPST61HjAFX7t9wohBhbNxVM_qux5YHf6Z-O2vo" />
+
+
+  <style>
+    body {
+      margin: 0;
+    }
+
+    iframe {
+      display: block;
+      background: #000;
+      border: none;
+      height: 100vh;
+      width: 100vw;
+    }
+  </style>
+
+</head>
+
+<body>
+
+
+  <!-- Google Analytics -->
+  <script>
+    window.ga = window.ga || function () {
+      (ga.q = ga.q || []).push(arguments)
+    };
+    ga.l = +new Date;
+    ga('create', 'UA-71773079-3', 'auto');
+    ga('send', 'pageview');
+  </script>
+  <script async src='https://www.google-analytics.com/analytics.js'></script>
+
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-JRJV04J4YL"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-JRJV04J4YL');
+  </script>
+</body>
+
+</html>
+''')
